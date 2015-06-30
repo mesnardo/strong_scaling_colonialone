2713c6e977f773c70e596caa3ddbc99a80c11e3c
mesnardo <mesnardo@gwu.edu>
2015-06-30 09:17:42 -0400

    MPICH-3.1.4 with FORTRAN libraries

---
+++ mpich-3.1.4-gnu-libf-opt/cavity3d/100x100x100/cg_nopc_cg_nopc_run1/n64/run_PetIBM3d.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+#SBATCH --job-name="n64"
+#SBATCH --output=log_summary_n64.out
+#SBATCH --error=log_summary_n64.err
+#SBATCH --partition=short
+#SBATCH --time=00:15:00
+#SBATCH -n 64
+
+
+module load mpich/3.1.4
+
+PETSC_DIR=$HOME/src/petsc/3.5.2
+PETSC_ARCH=linux-mpich-3.1.4-gnu-libf-opt
+
+PETIBM_DIR=$HOME/src/PetIBM
+PETIBM3D=$PETIBM_DIR/linux-mpich-3.1.4-gnu-libf-opt/PetIBM3d
+
+time mpirun $PETIBM3D -caseFolder . \
+	-sys1_pc_type none \
+	-sys2_pc_type none \
+	-log_summary
