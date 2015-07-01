c1012ac6b46f3d5df2f7c13b5793f82f3e78f40d
mesnardo <mesnardo@gwu.edu>
2015-07-01 10:53:49 -0400

    Add second run results for mpich-3.1.4 with FORTRAN libraries

---
+++ mpich-3.1.4-gnu-libf-opt/cavity3d/200x200x200/cg_nopc_cg_nopc_run2/n32/run_PetIBM3d.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+#SBATCH --job-name="n32"
+#SBATCH --output=log_summary_n32.out
+#SBATCH --error=log_summary_n32.err
+#SBATCH --partition=short
+#SBATCH --time=01:00:00
+#SBATCH -n 32
+
+
+module load mpich/3.1.4
+
+PETSC_DIR=$HOME/src/petsc/3.5.2
+PETSC_ARCH=linux-mpich-3.1.4-gnu-libf-opt
+
+PETIBM_DIR=$HOME/src/PetIBM
+PETIBM3D=$PETIBM_DIR/linux-mpich-3.1.4-gnu-libf-opt/PetIBM3d
+
+time mpirun $PETIBM3D -caseFolder . \
+	-sys1_pc_type none \
+	-sys2_pc_type none \
+	-log_summary
